8377a4aaf5f550423e7fd68ba7f6641504d00395
Baihua Chen <bchen102@myseneca.ca>
2024-10-27 12:50:42 -0400

    Extended

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough length!");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>,) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query.")
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path.")
+        };
 
         let ignore_case = env::var("IGNORE CASE").map_or(false, |var| var.eq("1"));
 
@@ -24,30 +28,11 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-    if result.is_empty() {
-        result.push("Not thing have been found.");
-    }
-    result
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_ci<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-    if result.is_empty() {
-        result.push("Not thing have been found.");
-    }
-    result
+    contents.lines().filter(|line| line.to_lowercase().contains(&query.to_lowercase())).collect()
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,9 @@ use std::process;
 use cli_practice::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
     // Deal with the err message that Result<_, _> returns
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Something bad happened. Check your code. Error message: {err}");
         process::exit(1);
     });
