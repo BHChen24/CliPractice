8bff4f34ecdcf394c4ac1396f9991c6db69aaf78
Baihua Chen <bchen102@myseneca.ca>
2024-10-27 11:59:42 -0400

    Finished

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ fn main() {
 
     // Deal with the err message that Result<_, _> returns
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Something bad happened. Check your code. Error message: {err}");
+        eprintln!("Something bad happened. Check your code. Error message: {err}");
         process::exit(1);
     });
     println!("Looking for '{}'", config.query);
     println!("in '{}'\n", config.file_path);
 
     if let Err(e) = cli_practice::run(config) {
-        println!("Error happened {e}");
+        eprintln!("Error happened {e}");
         process::exit(1);
     };
     // dbg!(args);
